84203b33e42d3c9e3d31daf0a1ef837e9f28f24a
Chen Yu <robin.chenyu@gmail.com>
2013-05-14 14:00:57 +0800

    alter email

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = 'bd3ef6af38a8311add5428e0f6ed0ae4135761fd5750bd790f8282eceab70d3f4f6271fb7e276e6b3dc39d40cecd50b98dfa5aa6591ac16ae7dc5b708d68ee96'
